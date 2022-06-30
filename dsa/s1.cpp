15f42748974f1e78803910a3c5a270044cc40ce2
RaisanJMR <raisanjmr@protonmail.com>
2022-06-30 08:54:42 +0530

    added stack

--- dsa/s1.cpp
+++ dsa/s1.cpp
@@ -1,9 +1,13 @@
+// Array based implementation of stack
 #include <iostream>
 using namespace std;
 
 #define MAX_SIZE 101
+
 int A[MAX_SIZE];
+
 int top = -1;
+
 void Push(int x)
 {
     if (top == MAX_SIZE - 1)
@@ -13,6 +17,7 @@ void Push(int x)
 
     A[++top] = x;
 }
+
 void Pop()
 {
     if (top == -1)
@@ -22,10 +27,28 @@ void Pop()
     }
     top--;
 }
-void Top() {
+// Return the value at top index
+int Top()
+{
     return A[top];
+}
+
+void Print()
+{
+    cout << "Stack is:" << endl;
+    for (int i = 0; i <= top; i++)
+    {
+        cout << A[i] << endl;
+    }
 }
 int main()
 {
+    Push(2);
+    Push(5);
+    Push(10);
+    Push(6);
+    Push(4);
+    Pop();
+    Print();
     return 0;
 }
